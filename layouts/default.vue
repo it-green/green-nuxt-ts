4ba1f0f6511433e92e3861ea55631055e8c4cfef
it-green <ryo.ctuad@gmail.com>
2020-04-04 21:52:14 +0900

    hozon

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,21 @@
 v-app
     .navigation
         v-app-bar(color='teal darken-1' dense dark)
-            v-app-bar-nav-icon
+            v-app-bar-nav-icon(@click.stop='drawer = !drawer')
             v-toolbar-title Nuxt TS App
-    v-icon home
-    v-icon info
+    v-navigation-drawer(v-model='drawer')
+        v-list(dense)
+            v-list-item(v-for='(item, i) in items' :key='i' link)
+                v-list-item-content
+                    v-list-item-title {{ item.title }}
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+
 @Component
 export default class Main extends Vue {
+    private drawer = null;
     private items = [
         { title: 'Home', icon: 'home' },
         { title: 'About', icon: 'info' }
